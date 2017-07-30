f10fd41ae3656ce68c317a1d2c2e07f46f8b66a3
Travis Weidenbenner <travis@thetravisw.com>
2017-07-29 23:31:51 -0500

    Add card component (more expirmenting with vue components)

--- src/pages/Rules.vue
+++ src/pages/Rules.vue
@@ -1,12 +1,20 @@
 <template>
   <div id="rules">
-    Rules
+    <card>
+      <h2>Rules<div class="add-on">Test</div></h2>
+      Testing
+    </card>
   </div>
 </template>
 
 <script>
+import Card from '../components/Card.vue'
+
 export default {
-  name: 'rules'
+  name: 'rules',
+  components: {
+    Card
+  }
 }
 </script>
 
