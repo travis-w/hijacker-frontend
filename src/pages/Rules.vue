20a9c6d6799b484cc6d5a7ea8c8be1c5498757fe
Travis Weidenbenner <travis@thetravisw.com>
2017-08-06 19:41:28 -0500

    Add modal component

--- src/pages/Rules.vue
+++ src/pages/Rules.vue
@@ -3,24 +3,36 @@
     <div class="row">
       <div class="twelve columns">
         <card>
-          <h2>Rules<div class="add-on" @click="addRule">Test</div></h2>
+          <h2>Rules<div class="add-on" @click="modals.newRule = true">Test</div></h2>
           <rule v-for="rule in rules" :rule="rule" :key="rule.path"></rule>
         </card>
       </div>
     </div>
-
+    <modal :open="modals.newRule" @closeBtn="modals.newRule = false">
+      <h4>New Rule</h4>
+      <p>New Rule Form Here</p>
+    </modal>
   </div>
 </template>
 
 <script>
 import Card from '../components/Card.vue'
 import Rule from '../components/Rule.vue'
+import Modal from '../components/Modal.vue'
 
 export default {
   name: 'rules',
+  data() {
+    return {
+      modals: {
+        newRule: false
+      }
+    }
+  },
   components: {
     Card,
-    Rule
+    Rule,
+    Modal
   },
   methods: {
     addRule() {
